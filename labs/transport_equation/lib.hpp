293f6d3d142a6bea751114033d5becd66cafa76b
DenRen <denrenruslan@gmail.com>
2022-04-06 20:28:14 +0300

    Добавил вычисление O(h), добился корректного результата

--- labs/transport_equation/lib.hpp
+++ labs/transport_equation/lib.hpp
@@ -1,6 +1,16 @@
 #include <vector>
 #include <cmath>
 
+#include "print_lib.hpp"
+
+#define HOST
+
+#ifdef HOST
+    #define DUMP(obj) std::cout << #obj ": " << obj << "\n"
+#else
+    #define DUMP(obj)
+#endif
+
 template <typename U_T_0, typename U_0_X, typename F,
           typename U_NEXT>
 std::vector <double>
@@ -24,13 +34,13 @@ calc_u (int num_points_coord_x,
     // Calc other u
     for (int time_i = 1; time_i < num_points_time; ++time_i) {
         double t = time_i * dt;
-        int cur_shift = time_i * num_points_coord_x;
-        int prev_shift = cur_shift - num_points_coord_x;
+        std::size_t cur_shift = time_i * num_points_coord_x;
+        std::size_t prev_shift = cur_shift - num_points_coord_x;
 
         double u_left = u[cur_shift] = u_t_0 (0);
-        for (int x_i = 0; x_i < num_points_coord_x; ++x_i) {
-            int prev_pos = prev_shift + x_i;
-            int cur_pos = cur_shift + x_i;
+        for (int x_i = 0; x_i < num_points_coord_x - 1; ++x_i) {
+            std::size_t prev_pos = prev_shift + x_i;
+            std::size_t cur_pos = cur_shift + x_i;
 
             double u_k_m  = u[prev_pos];
             double u_k_m1 = u[prev_pos + 1];
@@ -84,12 +94,11 @@ calc_u (int num_points_coord_x,
     std::vector <double> u (num_points_coord_x * num_points_time);
 
     for (int t_i = 0; t_i < num_points_time; ++t_i) {
-        std::size_t cur_shift = t_i * num_points_time;
+        std::size_t cur_shift = t_i * num_points_coord_x;
 
         double t = t_i * dt;
         for (int x_i = 0; x_i < num_points_coord_x; ++x_i) {
             double x = x_i * dx;
-
             u[cur_shift + x_i] = u_solve (t, x);
         }
     }
@@ -113,4 +122,105 @@ get_solve_ref (int num_points_coord_x,
 
     return calc_u (num_points_coord_x, num_points_time, dx, dt,
                    u_solve);
+}
+
+template <typename C>
+void
+check_eq_size (const C& cont_first,
+               const C& cont_second)
+{
+    if (cont_first.size () != cont_second.size ()) {
+        throw std::runtime_error ("Sizes of containers are difference");
+    }
+}
+
+template <typename T>
+T
+calc_err (const std::vector <T>& u_calc,
+          const std::vector <T>& u_ref)
+{
+    check_eq_size (u_calc, u_ref);
+
+    T max_err = 0;
+    for (std::size_t i = 0; i < u_calc.size (); ++i) {
+        T err = std::abs (u_calc[i] - u_ref[i]);
+        max_err = std::max (max_err, err);
+    }
+
+    return max_err;
+}
+
+//                        h      err
+std::vector <std::pair <double, double>>
+calc_h_err_vec (int num_points_coord_x_min,
+                int num_points_coord_dx,
+                int num_steps,
+                int num_points_time,
+                double x_max,
+                double t_max)
+{
+    std::vector <std::pair <double, double>> h_err (num_steps);
+
+    for (int step = 0; step < num_steps; ++step) {
+        int num_points_coord_x =
+            num_points_coord_x_min + num_points_coord_dx * step;
+
+        auto u = solve_transport_eq (num_points_coord_x,
+                                     num_points_time, x_max, t_max);
+        auto u_ref = get_solve_ref (num_points_coord_x,
+                                    num_points_time, x_max, t_max);
+        
+        double h = x_max / (num_points_coord_x - 1);
+        double err = calc_err (u, u_ref);
+        h_err[step] = {h, err};
+    }
+
+    return h_err;
+}
+
+// y = k*x + b
+// return k, b
+template <typename T>
+std::pair <T, T>
+linearize (const std::vector <T>& x,
+           const std::vector <T>& y)
+{
+    check_eq_size (x, y);
+
+    const auto n = x.size ();
+
+    T sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0;
+    for (std::size_t i = 0; i < n; ++i) {
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += x[i] * y[i];
+        sum_x2 += std::pow (x[i], 2);
+    }
+ 
+    T k = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - std::pow (sum_x, 2));
+    T b = (sum_y - k * sum_x) / n;
+    
+    return {k, b};
+}
+
+template <typename T>
+std::pair <T, T>
+linearize (const std::vector <std::pair <T, T>>& x_y)
+{
+    const auto n = x_y.size ();
+
+    T sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0;
+    for (std::size_t i = 0; i < n; ++i) {
+        auto[x, y] = x_y[i];
+
+        sum_x += x;
+        sum_y += y;
+        sum_xy += x * y;
+        sum_x2 += std::pow (x, 2);
+    }
+ 
+    T k = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - std::pow (sum_x, 2));
+    T b = (sum_y - k * sum_x) / n;
+    
+    return {k, b};
 }
--- labs/transport_equation/main.cpp
+++ labs/transport_equation/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 
-#include "print_lib.hpp"
 #include "lib.hpp"
 
 int main () {
     std::ios_base::sync_with_stdio (false);
 
-    unsigned M = 10000 / 10; // coord
-    unsigned K = 10000 / 10; // time
+    unsigned M = 10; // coord
+    unsigned K = 100000; // time
 
-    double X_max = 1; // X_min = 0
-    double T_max = 2;  // T_min = 0
+    double X_max = 10;  // X_min = 0
+    double T_max = 1; // T_min = 0
 
-    auto u = solve_transport_eq (M, K, X_max, T_max);
-    auto u_ref = get_solve_ref (M, K, X_max, T_max);
+    // auto u = solve_transport_eq (M, K, X_max, T_max);
+    // auto u_ref = get_solve_ref (M, K, X_max, T_max);
 
-    std::cout << u << '\n';
-    std::cout << u_ref << '\n';
+    // std::cout << u << '\n';
+    // std::cout << u_ref << '\n';
 
-    for (int i = 0; i < u.size (); ++i)
-        std::cout << std::fabs (u[i] - u_ref[i]) << ' ';
+    auto h_err = calc_h_err_vec (M, M / 4, 50, K, X_max, T_max);
+    for (auto[h, err] : h_err) {
+        // std::cout << std::log (h) << ' ' << std::log (err) << '\n';
+    }
+
+    for (auto&[h, err] : h_err) {
+        h = std::log (h);
+        err = std::log (err);
+    }
+    std::cout << "O(h^" << linearize (h_err).first << ")\n";
 }
